30511e800fc54fe58055772bcbd7094148aba802
nakmaykin <nakmaykin@sfxdx.com>
2025-02-28 09:53:39 +0700

    feat: adds creation and retrieval of tasks via the database

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-type RequestBody struct {
-	Task string `json:"task"`
+type Response struct {
+	Message string `json:"message"`
 }
 
-var task string
-
 func HelloHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "hello, %s", task)
+	var tasks []Task
+
+	if err := DB.Find(&tasks).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var taskInput RequestBody
+	var taskInput Task
 
 	err := json.NewDecoder(r.Body).Decode(&taskInput)
 	if err != nil {
@@ -27,17 +32,23 @@ func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	task = taskInput.Task
+	if err := DB.Create(&taskInput).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	response := "Task: " + task + " successfully created"
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(response))
+	json.NewEncoder(w).Encode(Response{Message: "Task successfully created"})
 }
 
 func main() {
+	InitDB()
+
+	DB.AutoMigrate(&Task{})
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/task", HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/api/tasks", HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/api/create-task", CreateTaskHandler).Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe("localhost:9090", r))
