a257c7cd3ef294925e6879331f50c0f31c347bed
nakmaykin <nakmaykin@sfxdx.com>
2025-02-28 09:53:09 +0700

    feat: adds a db initialization function

---
+++ db.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "host=localhost user=postgres password=NIKITA100 dbname=postgres port=5432 sslmode=disable"
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	} else {
+		fmt.Println("Database connection successful")
+	}
+}
