4776080c547301c24fb23aa8cbc1259784e187f5
nakmaykin <nakmaykin@sfxdx.com>
2025-02-28 17:20:17 +0700

    feat: adds task update and delete handlers

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Response struct {
@@ -15,7 +16,7 @@ func GetTasksHandler(w http.ResponseWriter, _ *http.Request) {
 	var tasks []Task
 
 	if err := DB.Find(&tasks).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
@@ -37,8 +38,56 @@ func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(taskInput)
+}
+
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := mux.Vars(r)["id"]
+
+	formattedId, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Response{Message: "Invalid id sent"})
+		return
+	}
+
+	var updatedTask Task
+
+	err = json.NewDecoder(r.Body).Decode(&updatedTask)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := DB.Model(&Task{}).Where("id = ?", formattedId).Updates(updatedTask).Error; err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(Response{Message: "task with the specified id does not exist"})
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Response{Message: "Task successfully created"})
+	json.NewEncoder(w).Encode(Response{Message: "Task successfully updated"})
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := mux.Vars(r)["id"]
+
+	formattedId, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Response{Message: "Invalid id sent"})
+		return
+	}
+
+	if err := DB.Delete(&Task{}, formattedId).Error; err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(Response{Message: "task with the specified id does not exist"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Response{Message: "Task successfully deleted"})
 }
 
 func main() {
@@ -50,6 +99,8 @@ func main() {
 
 	r.HandleFunc("/api/tasks", GetTasksHandler).Methods(http.MethodGet)
 	r.HandleFunc("/api/create-task", CreateTaskHandler).Methods(http.MethodPost)
+	r.HandleFunc("/api/update-task/{id:[0-9]+}", UpdateTaskHandler).Methods(http.MethodPatch)
+	r.HandleFunc("/api/delete-task/{id:[0-9]+}", DeleteTaskHandler).Methods(http.MethodDelete)
 
 	log.Fatal(http.ListenAndServe("localhost:9090", r))
 }
