8861e056e883a69fb831f02de95c556c9e11f475
nakmaykin <nakmaykin@sfxdx.com>
2025-03-27 07:11:28 +0700

    feat: register new handlers

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,7 +4,9 @@ import (
 	"github.com/Arenelin/Todo-list/internal/database"
 	"github.com/Arenelin/Todo-list/internal/handlers"
 	"github.com/Arenelin/Todo-list/internal/taskService"
+	"github.com/Arenelin/Todo-list/internal/userService"
 	"github.com/Arenelin/Todo-list/internal/web/tasks"
+	"github.com/Arenelin/Todo-list/internal/web/users"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"log"
@@ -13,17 +15,24 @@ import (
 func main() {
 	database.InitDB()
 
-	repo := taskService.NewTaskRepository(database.DB)
-	service := taskService.NewService(repo)
-	handler := handlers.NewHandler(service)
+	tasksRepo := taskService.NewTaskRepository(database.DB)
+	tasksService := taskService.NewTaskService(tasksRepo)
+	tasksHandler := handlers.NewTaskHandler(tasksService)
+
+	usersRepo := userService.NewUserRepository(database.DB)
+	usersService := userService.NewUserService(usersRepo)
+	usersHandler := handlers.NewUserHandler(usersService)
 
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	strictHandler := tasks.NewStrictHandler(handler, nil)
-	tasks.RegisterHandlers(e, strictHandler)
+	strictTasksHandler := tasks.NewStrictHandler(tasksHandler, nil)
+	tasks.RegisterHandlers(e, strictTasksHandler)
+
+	strictUsersHandler := users.NewStrictHandler(usersHandler, nil)
+	users.RegisterHandlers(e, strictUsersHandler)
 
 	if err := e.Start(":9090"); err != nil {
 		log.Fatalf("failed to start with err: %v", err)
